69ef5bfde142da6b2cd751dbf3292a8baa3aa89c
AnthonyWGit <anthony.wetzstein@gmail.com>
2025-08-29 09:10:22 +0200

    fidling with imports and calling methods

---
+++ src/com/secondarypackage/NewClass.java
@@ -0,0 +1,11 @@
+package com.secondarypackage;
+import com.somepackage.*;
+
+public class NewClass {
+    public void exampleFunction(){
+        Main.main(new String[0]);
+    }
+    public static void callingLines(int nb){
+        Main.printLines(nb);
+    }
+}
--- src/com/somepackage/Main.java
+++ src/com/somepackage/Main.java
@@ -1,10 +1,18 @@
 //This will allow us to import as a package all the classes and their methods below
 package com.somepackage;
+import com.secondarypackage.*;
 
 public class Main {
     //Main function should always be static and public to be accessible 
     public static void main(String[] args){
         System.out.println("Hello world");
+        com.secondarypackage.NewClass.callingLines(5);
+    }
+
+    public static void printLines(int numberOfLines){
+        for (int i=0; i < numberOfLines; i++) {
+            System.out.println("Testing lines");
+        }
     }
 }
 
