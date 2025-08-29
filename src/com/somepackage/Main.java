3ad9b0ef11674c07fe15e6c0ce3d76dd88fe3b11
AnthonyWGit <anthony.wetzstein@gmail.com>
2025-08-29 07:25:03 +0200

    adding source files

---
+++ src/com/somepackage/Main.java
@@ -0,0 +1,10 @@
+//This will allow us to import as a package all the classes and their methods below
+package somepackage;
+
+public class Main {
+    //Main function should always be static and public to be accessible 
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
+
